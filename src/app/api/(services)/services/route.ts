233d58873c21dff7c9383007db367f38b96669ef
Lucase1840 <l.ucase1840@gmail.com>
2025-06-21 20:35:40 -0300

    refactor(services-api): services validation schemas were added
    - Validation schemas and types were updated for services api.

--- src/app/api/(services)/services/route.ts
+++ src/app/api/(services)/services/route.ts
@@ -1,21 +1,29 @@
 import { NextResponse } from 'next/server'
 
-import { type ServicesData } from '@/types/services-data-types'
+import { ZodError } from 'zod'
+
+import { ServicesSchema } from '@/lib/validation-schemas/services-validation-schemas'
 
 import services from '../data/mock-data.json'
 
 export async function GET() {
   try {
-    const servicesData = new Promise<ServicesData>((resolve) =>
-      setTimeout(() => resolve(services as ServicesData), 3000),
+    const getServicesData = new Promise<unknown>((resolve) =>
+      setTimeout(() => resolve(services), 3000),
     )
 
-    const data = await servicesData
+    const data = await getServicesData
 
-    return NextResponse.json(data.services)
+    const servicesData = ServicesSchema.parse(data)
+
+    return NextResponse.json(servicesData.services)
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+
+    if (error instanceof ZodError) {
+      return NextResponse.json({ error: 'Received invalid data from upstream' }, { status: 422 })
     }
 
     return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
